1fd24daefed98ee35cb31e5bee584a28d3ffe064
Daniel Hidalgo <jidalgo11@gmail.com>
2024-12-06 15:27:53 -0700

    setting up blog routes

--- backend-projects/personal-blog/routes/admin.js
+++ backend-projects/personal-blog/routes/admin.js
@@ -1,35 +1,15 @@
-const path = require("path");
-
 const express = require("express");
-const { getArticles } = require("../models/articleModels");
+
+const articlesController = require("../controllers/articles");
 
 const router = express.Router();
 
-router.get("/admin", (req, res, next) => {
-	console.log(getArticles());
-	const articles = getArticles();
-	console.log(articles);
-	res.render("admin", {
-		pageTitle: "Personal Blog - Admin",
-		articles: articles,
-	});
-});
+router.get("/admin", articlesController.getAdminArticles);
 
-router.get("/edit-article", (req, res, next) => {
-	res.sendFile(
-		path.join(__dirname, "..", "views", "admin", "edit-article.html")
-	);
-});
+router.get("/edit-article", articlesController.getEditArticle);
 
-router.get("/add-article", (req, res, next) => {
-	res.sendFile(
-		path.join(__dirname, "..", "views", "admin", "add-article.html")
-	);
-});
+router.get("/add-article", articlesController.getAddArticle);
 
-router.post("/add-article", (req, res, next) => {
-	console.log(req.body);
-	res.redirect("/admin");
-});
+router.post("/add-article", articlesController.postAddArticle);
 
 module.exports = router;
